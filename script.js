46c8c4155e94bdf8f9c868b1e795fa78070c66b0
Adarsh11234 <aadarshpatil123@gmail.com>
2025-02-03 18:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const codes = document.querySelectorAll('.code');
 
 codes.forEach((code, idx) => {
-    code.addEventListener('input', (e) => {
-        if (e.target.value.length === 1 && idx < codes.length - 1) {
-            codes[idx + 1].focus();
-        }
-    });
-
     code.addEventListener('keydown', (e) => {
         if (e.key === 'Backspace' && idx > 0 && !e.target.value) {
-            codes[idx - 1].focus();
+            codes[idx - 1].focus(); // Focus on the previous input field
+            codes[idx - 1].value = ''; // Clear the previous input field
         }
     });
 });
