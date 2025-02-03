595581b72f7e77048cfe9880362531aa323c3030
Adarsh11234 <aadarshpatil123@gmail.com>
2025-02-03 18:33:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-const codes = document.querySelectorAll(".code");
+const codes = document.querySelectorAll('.code');
 
-codes[0].focus();
-
-codes.forEach((code, index) => {
-    code.addEventListener("input", (e) => {
-        const value = e.target.value;
-        
-       
-        if (value && index < codes.length - 1) {
-            codes[index + 1].focus();
+codes.forEach((code, idx) => {
+    code.addEventListener('input', (e) => {
+        if (e.target.value.length === 1 && idx < codes.length - 1) {
+            codes[idx + 1].focus();
         }
     });
 
-    code.addEventListener("keydown", (e) => {
-        if (e.key === "Backspace" && index > 0) {
-            codes[index].value = "";
-            codes[index - 1].focus();
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && idx > 0 && !e.target.value) {
+            codes[idx - 1].focus();
         }
     });
 });
